5f761b6ff7fc0761e1cc70c9542794c29fbc7bdd
Nassim Ouali <100594579+Nasss10@users.noreply.github.com>
2023-04-24 14:58:53 +0200

    Add files via upload

--- src/views/jeu.vue
+++ src/views/jeu.vue
@@ -7,12 +7,12 @@ import axios from 'axios'
 const route = useRoute()
 const idPartie = ref()
 const partie = ref()
-const idUser = ref()
+const iduser = ref()
 
 
 onMounted(async () => {
   idPartie.value = route.params.idgame
-  idUser.value = route.params.iduser
+  iduser.value = route.params.iduser
 
   partie.value = await fetchApi()
   setInterval(async () => {
@@ -49,7 +49,7 @@ function cartetrigger(index){
   
 
 
-  if (this.idUser == 1) {
+  if (this.iduser == 1) {
     console.log(this.partie.motParties[index].couleurJ1)
 
     if (this.partie.motParties[index].couleurJ2 === 'Vert') {
@@ -113,7 +113,7 @@ function cartetrigger(index){
     
 
 
-  if (this.idUser == 2) {
+  if (this.iduser == 2) {
     console.log(this.partie.motParties[index].couleurJ2)
 
     if (this.partie.motParties[index].couleurJ1 === 'Vert') {
@@ -229,7 +229,9 @@ function cartetrigger(index){
           })
           .catch(error => {
             console.error(error)
-          })
+          }),
+          window.location.href = 'https://mmi21a11.sae401.ovh/compte';
+
   }
 
   function perdregame(){
@@ -242,7 +244,9 @@ function cartetrigger(index){
           })
           .catch(error => {
             console.error(error)
-          })
+          }),
+          window.location.href = 'https://mmi21a11.sae401.ovh/compte';
+
   }
 
 
@@ -252,14 +256,18 @@ function cartetrigger(index){
 </script>
 
 <template>
-  <body>
-    
-  <h1>Bienvenue dans la partie {{ idPartie }}</h1>
+  <div class="wrapper2">
+  <div class="indicationdiv" v-if="iduser == 1 && partie.tourduj == '2'" > <p><span class="indication">Fournis un indice pour le joueur 2</span></p> </div>
+  <div class="indicationdiv" v-if="iduser == 1 && partie.tourduj == '1'" > <p><span class="indication">Attend l'indice pour commencer à deviner</span></p></div>
+  <div class="indicationdiv" v-if="iduser == 2 && partie.tourduj == '1'" ><p><span class="indication">Fournis un indice pour le joueur 1</span></p> </div>
+  <div class="indicationdiv" v-if="iduser == 2 && partie.tourduj == '2'" ><p><span class="indication">Attend l'indice pour commencer à deviner</span></p> </div>
+</div>
 <div class="wrappper">
+  
   <div v-if="partie" class="cart">
-    <p>Carte "indice"</p>
+    <p><span class="carteindigice">Carte "indice"</span></p>
     <div v-for="mot in partie.motParties" :key="mot.id">
-      <div v-if="idUser === '1'" :class="`case ${ mot.couleurJ1 }`">
+      <div v-if="iduser === '1'" :class="`case ${ mot.couleurJ1 }`">
         &nbsp;
       </div>
       <div v-else :class="`case ${ mot.couleurJ2 }`">
@@ -270,13 +278,14 @@ function cartetrigger(index){
   <div v-if="partie" class="parent">
     
     
-    <div v-if="idUser == 1 && partie.tourduj == '2'" class="notyourturn"></div>
+    <div v-if="iduser == 1 && partie.tourduj == '2'" class="notyourturn"></div>
 
-    <div v-if="idUser == 2 && partie.tourduj == '1'" class="notyourturn"></div>
+    <div v-if="iduser == 2 && partie.tourduj == '1'" class="notyourturn"></div>
 
     <div  v-for="(mot, index) in partie.motParties" @click="cartetrigger(index)" :key="mot.id">
 
-      <div v-if="idUser === '1'" class="couleurdecarte" :class="`${mot.etatmot} ${mot.couleurJ1}`">
+      <div v-if="iduser === '1'" class="couleurdecarte" :class="`${mot.etatmot} ${mot.couleurJ1}`"> <!-- début de la div pr les mots -->
+
         <div v-if="mot.etatmot === 'trouver'" class="couleurdecarte">TROUVER</div>
         <div v-if="mot.etatmot === 'jeton'" class="couleurdecarte">Jeton</div>
         <div v-if="mot.etatmot === 'perdu'" class="endgame"><p>Partie Perdu vous avez trouver une carte noir</p> <button @click="perdregame">Finir Partie</button></div>
@@ -285,7 +294,7 @@ function cartetrigger(index){
 
 
         <p class="texte">{{ mot.mot.mot }}</p> <!-- le premier mot est la boucle, le deuxième mot est la relation entre PartieMot et Mot, et le troisième mot est le champs dans mot -->
-      </div>
+      </div><!-- Fin de la div pr les mots -->
 
       <div v-else class="couleurdecarte" :class="mot.couleurJ2">
         <div v-if="mot.etatmot === 'trouver'" class="couleurdecarte">TROUVER</div>
@@ -300,11 +309,13 @@ function cartetrigger(index){
     </div>
     
   </div>
+
+
   
   <div class="historiquemessage">
   <p class="indicee">Les indicices</p>
 <div class="trait"></div>
-<div v-if="partie" class="montxt">
+<div v-if="partie" class="montxpt">
   <div v-for="indice in partie.indices.slice().reverse()" :key="indice.id" class="bonindices">
     <p>  Voici votre indice : <span class="indice">{{indice.mot}}</span> </p>
   </div>
@@ -320,36 +331,36 @@ function cartetrigger(index){
 
   
   
-  <div v-if="idUser == 1 && partie.tourduj == '2'" >Ton pote est en train de deviner </div>
-  <div v-if="idUser == 1 && partie.tourduj == '1'" >Veuillez attendre de recevoir l'indice pour jouer </div>
-  <div v-if="idUser == 2 && partie.tourduj == '1'" >Ton pote est en train de deviner </div>
-  <div v-if="idUser == 2 && partie.tourduj == '2'" >Attend l'indice et vasy devine mon sauce </div>
-
-
-
-<div v-if="partie" class="montxt">
-    <div v-if="idUser == 2 && partie.tourduj == '1'" >
-
   
-</div>
-</div>
+
+
+<div class="wrapper3">
+
+
+
+
+
+<p><span class="titleajt">Ajouter un indice</span></p>
+      <div>
+        <input type="text" id="mot" v-model="mot" placeholder="Indice(s) - 1"/>
+      </div>
+      
+      <button @click="ajouter">Ajouter</button>
+
+
+
 <div v-if="partie" class="montxt">
-    <div v-if="idUser == 1 && partie.tourduj == '2'" >
+  <div v-if="iduser == 1 && partie.tourduj == '1'" >
 
-</div>
-</div>
-<div v-if="partie" class="montxt">
-  <div v-if="idUser == 1 && partie.tourduj == '1'" >
-
-  <button @click="passageTour">Fini de devinez akhy</button>
+  <button @click="passageTour">J'ai fini de deviner</button>
 </div>
 
 </div>
 
 <div v-if="partie" class="montxt">
-  <div v-if="idUser == 2 && partie.tourduj == '2'" >
+  <div v-if="iduser == 2 && partie.tourduj == '2'" >
 
-  <button @click="passageTourj2">Fini de devinez akhy</button>
+  <button @click="passageTourj2">J'ai fini de deviner</button>
 </div>
 
 
@@ -358,28 +369,88 @@ function cartetrigger(index){
 
 <div v-if="partie.tourpartie == 9" class="endgame" > Fin de partie 9 tours se sont écoulé</div>
 </div>
-
-
-
-<h2>Ajouter un indice</h2>
-      <div>
-        <label for="nom">Votre indice</label>
-        <input type="text" id="mot" v-model="mot" placeholder="Indice(s) - 1"/>
-      </div>
-      
-      <button @click="ajouter">Ajouter</button>
     
     
-
+</div>
       
      
 
 
-</body>
 </template>
 
 <style scoped>
+.montxpt{
+  margin-left: 10px;
+}
 
+.titleajt{
+  color: aliceblue;
+  font-size: 2.3rem;
+  margin-right: 20px;
+}
+.montxt{
+  margin-left: 50px;
+}
+.montxt button{
+  padding: 1.3em 3em;
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-weight: 500;
+  color: #000;
+  background-color: #fff;
+  border: none;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  cursor: pointer;
+  outline: none;
+}
+.montxt button:hover{
+  background-color: #23c483;
+  box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+  color: #fff;
+  transform: translateY(-7px);
+
+}
+.wrapper3{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.carteindigice{
+  color: aliceblue;
+  font-size: 20px;
+}
+.wrapper2{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.indicationdiv{
+  width: 80%;
+  height: 8vh;
+  background-color: antiquewhite;
+  border-radius: 10px;
+  margin-bottom: 30px;
+}
+.indication{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height:100%;
+  width: 100%;
+  color: black;
+  font-size: 30px;
+  text-align: center;
+}
+body{
+  background-color: #1e1e1e;
+}
 .bonindices{
   font-size: 1.3rem;
 }
@@ -400,7 +471,7 @@ width: 100%;
 
 }
 .historiquemessage{
-  background-color: aqua;
+  background: radial-gradient(343px at 46.3% 47.5%, rgb(242, 242, 242) 0%, rgb(241, 241, 241) 72.9%);
   border-radius: 20px;
   width: 20vw;
   height: 40vh;
@@ -441,7 +512,8 @@ width: 100%;
 
 .notyourturn {
   position: absolute;
-    background-color: #0000004d;
+    background-color: #000000;
+    opacity: 90%;
     width: 100%;
     height: 100%;
     pointer-events: none;
@@ -457,6 +529,7 @@ width: 100%;
     border: solid black 1px;
     padding: 1rem;
     position: relative;
+    background-color: aliceblue;
 }
 .couleurdecarte{
  border-radius: 10px;
@@ -469,18 +542,21 @@ width: 100%;
 }
 
 .texte{
-  font-size: 1rem;
-  color: blue;
+  font-size: 1.3rem;
+  color: black;
 }
 .Neutre{
-  background-color: orange;
-}
+background-color: #ECEBBD;}
 .Noir{
   background-color: black;
   color: white;
 }
+.Noir .texte {
+  color: white;
+}
+
 .Vert{
-  background-color: darkgreen;
+  background-color: #23c483;
 }
 .carte {
   float: left;
@@ -498,7 +574,8 @@ width: 100%;
 }
 
 .case.Vert {
-  background-color: #00FF00;
+  background-color: #23c483;
+  
 
 }
 
